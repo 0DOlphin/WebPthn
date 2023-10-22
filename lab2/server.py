3bca3218dbab63863ff7433d24740278fc6d8c99
0DOlphin <94777235+0DOlphin@users.noreply.github.com>
2023-10-22 10:02:50 +0300

    Delete lab2 directory

--- lab2/server.py
+++
@@ -1,6 +0,0 @@
-from http.server import  HTTPServer, CGIHTTPRequestHandler
-adders = ("localhost", 5050)
-
-server = HTTPServer(adders, CGIHTTPRequestHandler)
-print("Є контакт...")
-server.serve_forever()
