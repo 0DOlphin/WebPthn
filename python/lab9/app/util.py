d96fcc8af06ec3243853be97b696c18a09738edf
0DOlphin <94777235+0DOlphin@users.noreply.github.com>
2023-11-12 16:20:53 +0200

    add lab9 & screens

---
+++ python/lab9/app/util.py
@@ -0,0 +1,23 @@
+from app import app
+from PIL import Image
+import secrets
+import os
+
+
+def save_picture(form_picture):
+    hex = secrets.token_hex(8)
+    _, ext = os.path.splitext(form_picture.filename)
+    picture_fn = hex + ext
+    picture_path = os.path.join(app.root_path, 'static', 'profile_pics', picture_fn)
+
+    # Log paths for debugging
+    print("Picture Path:", picture_path)
+    print("Absolute Path:", os.path.abspath(os.path.join(app.root_path, 'static', 'profile_pics')))
+
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.abspath(os.path.join(app.root_path, 'static', 'profile_pics')), exist_ok=True)
+
+    image = Image.open(form_picture)
+    image.thumbnail((150, 150))
+    image.save(picture_path)
+    return picture_fn
